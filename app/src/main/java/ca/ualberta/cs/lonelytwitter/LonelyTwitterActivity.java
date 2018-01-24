f693ecd6f51d4dbe933d84d160200bbcc4c0e306
Vincentlcy <liuchuyang1997@outlook.com>
2018-01-23 18:53:21 -0700

    clear button can used

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,34 +1,45 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class LonelyTwitterActivity extends Activity {
 
-	private static final String FILENAME = "file.sav";
+	private static final String FILENAME = "tweets.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
-	private ArrayList<LonelyTwittermood> moods;
-	
+
+	private ArrayList<Tweet> tweetList;
+	private ArrayAdapter<Tweet> adapter;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.i("LifeCycle ---->", "onCreate is called");
 		setContentView(R.layout.main);
 
 		bodyText = (EditText) findViewById(R.id.body);
@@ -40,59 +51,86 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
-				moods.add(new HappyMood());
-				moods.add(new SadMood());
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
 
+				Tweet tweet = new NormalTweet(text);
+				tweetList.add(tweet);
+
+				adapter.notifyDataSetChanged();
+
+				saveInFile();
 			}
 		});
 	}
 
 	@Override
 	protected void onStart() {
+
 		// TODO Auto-generated method stub
 		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
+		Log.i("LifeCycle --->", "onStart is called");
+
+		loadFromFile();
+
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
 		oldTweetsList.setAdapter(adapter);
+
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	private void loadFromFile() {
+
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+
+			Gson gson = new Gson();
+
+			// Taken https://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
+			// 2018-01-23
+			Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+			tweetList = gson.fromJson(in, listType);
+
+		} catch (FileNotFoundException e) {
+			tweetList = new ArrayList<Tweet>();
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+
+	}
+
+	private void saveInFile() {
+		try {
+
+			FileOutputStream fos = openFileOutput(FILENAME,
+					Context.MODE_PRIVATE);
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+			Gson gson = new Gson();
+			gson.toJson(tweetList, out);
+			out.flush();
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException();
 		}
-		return tweets.toArray(new String[tweets.size()]);
 	}
-	
-	private void saveInFile(String text, Date date) {
-		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
-			fos.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.i("Lifecycle", "onDestroy is called");
+	}
+
+	// new class for clear
+	public void clear(View view) {
+		loadFromFile();
+		tweetList.clear();
+		adapter.clear();
+		saveInFile();
+		loadFromFile();
+		onStart();
 	}
 }
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by dezfuli on 1/16/18.
+ */
+import java.util.Date;
+
+public interface Tweetable {
+    public String getMessage();
+    public Date getDate();
+}
